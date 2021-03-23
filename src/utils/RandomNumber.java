a657a0809e4131768411cc2b6990cfa657d5c073
Alessandro <alessandro.parrinoap@gmail.com>
2021-03-23 16:07:07 +0100

    Random int

--- src/utils/RandomNumber.java
+++ src/utils/RandomNumber.java
@@ -10,7 +10,13 @@ import java.util.Random;
 
 public class RandomNumber {
     public static int randomInt(int min, int max) {
+        if(max < min || (min == 0 && max == 0))
+            throw new IllegalArgumentException("Illegal interval");
         Random r = new Random();
-        return r.nextInt((max - min) + 1) + min;
+        int randomNumber;
+        do {
+            randomNumber = r.nextInt((max - min) + 1) + min;
+        } while(randomNumber == 0);
+        return randomNumber;
     }
 }
